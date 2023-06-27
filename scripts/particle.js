ff5b907b3af187dee96b5a06fddbe0428a9e69cf
claualarcon94 <claualarcon94@hotmail.com>
2023-06-27 03:09:27 -0400

    initialize files

---
+++ scripts/particle.js
@@ -0,0 +1,78 @@
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 4;
+  this.h = 0;
+  this.prevPos = this.pos.copy();
+
+
+  this.update = () => {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+
+  };
+
+  this.recieveField = (field) => {
+    let x, y;
+    x = Math.floor(this.pos.x / scl);
+    y = Math.floor(this.pos.y / scl);
+    this.applyForce(field[x][y]);
+
+  };
+
+  this.applyForce = (force) => {
+
+    this.acc.add(force);
+
+  };
+
+  this.show = () => {
+    //stroke(0,0,255);
+    stroke(0,150,150);
+   /* 
+    stroke(this.h, 255, 255, 25);
+    this.h = this.h + 1;
+    if (this.h > 255) {
+      this.h = 0;
+    }
+    strokeWeight(1);
+    */
+    strokeWeight(0.1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    //point(this.pos.x,this.pos.y);
+    this.updatePrev();
+    //console.log(this.pos);
+  };
+
+  this.updatePrev = () => {
+    this.prevPos.set(this.pos);
+  };
+  
+
+  this.edges = () => {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+    
+  };
+
+  
+
+}
+
---
+++ scripts/sketch.js
@@ -0,0 +1,108 @@
+var inc = 0.1;
+var scl = 20;
+var cols, rows;
+var fr;
+
+var field = [];
+var particles = [];
+var zoff = 0;
+var vectors = false;
+var size;
+
+
+function setup() {
+  size = 800;
+  canvas = createCanvas(size, size);
+  canvas.parent(document.getElementById('canvas-container'));
+  //colorMode(HSB, 255);
+  //pixelDensity(2);
+  //background(150);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = document.querySelector('.js-frameRate');
+
+  //field = new Array(cols * rows);
+
+  for (let i = 0; i < 200; i++) {
+
+    particles[i] = new Particle();
+  }
+  loadPixels();
+  noiseSeed(99);
+  background(255);
+}
+
+function draw() {
+
+
+
+
+  var yoff = 0;
+  for (var y = 0; y <= rows; y++) {
+    var xoff = 0;
+    const row = [];
+    for (var x = 0; x <= cols; x++) {
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var vector = p5.Vector.fromAngle(angle);
+      vector.setMag(1);
+      row.push(vector);
+      xoff += inc;
+
+
+      if (vectors) {
+        stroke(0);
+        strokeWeight(1);
+        rect(x * scl, y * scl, scl, scl);
+
+        push();
+
+        translate(x * scl, y * scl);
+        rotate(vector.heading());
+        strokeWeight(1);
+        line(0, 0, scl, vector.y);
+        pop();
+      }
+
+
+
+
+    }
+
+    yoff += inc;
+    field.push(row);
+    zoff += 0.0002;
+
+  }
+
+
+  for (let particle of particles) {
+    particle.recieveField(field);
+    particle.update();
+    particle.edges();
+    if (!vectors) {
+      particle.show();
+    }
+
+  }
+  field.splice(0, field.length);
+  fr.innerHTML = floor(frameRate());
+
+
+
+
+
+}
+
+function keyPressed() {
+  // Check if the key pressed is "g"
+  if (keyCode === 71) {
+    // Perform the action when "g" key is pressed
+    vectors = !vectors;
+    console.log('The "g" key is pressed!');
+    background(255);
+
+  }
+}
+
+
